361d7d334796a52a9ad30c510c2b9e2955f8e847
NourEldin <noureldin.osama.saad@gmail.com>
2023-01-19 13:13:42 +0200

    Refactor script, added type hints, comments and docstring for function

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 from pptx import Presentation
 from pyshorteners import Shortener
 
-FILE_NAME = 'PYPPT.pptx'
-prs = Presentation(FILE_NAME)
-shortener = Shortener().tinyurl
 
-for slide in prs.slides:
-    for shape in slide.shapes:
-        if not shape.has_text_frame:
-            continue
-        for paragraph in shape.text_frame.paragraphs:
-            for run in paragraph.runs:
-                address = run.hyperlink.address
-                if address is None:
-                    continue
-                # print(run.text)
-                try:
-                    run.text = shortener.short(address)
-                except:
-                    continue
-                # print(address)
-prs.save(FILE_NAME)
+def shorten_ppt_urls(file_name: str) -> None:
+    """
+    This function shortens all URLs in a PowerPoint presentation using the TinyURL service.
+
+    Args:
+    file_name (str): The name of the PowerPoint file to be modified.
+
+    Returns:
+    None
+    """
+
+    # Open the PowerPoint file
+    prs = Presentation(file_name)
+
+    # Create a shortener object using the TinyURL service
+    shortener = Shortener().tinyurl
+
+    # Iterate through all the slides, shapes, and runs in the presentation
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            # Check if the shape has a text frame
+            if not shape.has_text_frame:
+                continue
+            for paragraph in shape.text_frame.paragraphs:
+                for run in paragraph.runs:
+                    # Get the hyperlink address
+                    address = run.hyperlink.address
+
+                    # Check if the address is not None
+                    if address is None:
+                        continue
+
+                    # Try to shorten the URL
+                    try:
+                        run.text = shortener.short(address)
+                    except:
+                        continue
+
+    # Save the changes to the original PowerPoint file
+    prs.save(file_name)
+
+
+# Use the function
+FILE_NAME = 'PYPPT.pptx'
+shorten_ppt_urls(FILE_NAME)
